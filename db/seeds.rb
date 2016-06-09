8a957d15e12cd99b9978ec11b71fe7696d7d32ce
Justincadburywong <justincadburywong@gmail.com>
2016-06-09 15:31:35 -0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@ User.create(first_name: "erica", last_name: "freeman", password_hash: "123", ema
 	User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password_hash: "123", email: Faker::Internet.email, street: Faker::Address.street_address, city: Faker::Address.city, state: Faker::Address.state, zip: Faker::Address.zip))
 end
 # Event.create(event_name: "movie night", location: "423 E Blithedale Ave, Mill Valley, Ca 94941", date: 2016-06-15, start_time: 17-00-00, end_time: 21-00-00, host_id: 2)
+
+50.times do (
+	Event.create(event_name: Faker::Company.catch_phrase, location: Faker::Address.street_address, start_time: Faker::Time.forward(rand(1..5), :morning), end_time: Faker::Time.forward(rand(6..9), :evening), host_id: rand(1..20)))
+	end
