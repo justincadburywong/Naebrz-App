730b837d275b2882acddcaff078c61b755efeae8
Justincadburywong <justincadburywong@gmail.com>
2016-06-09 12:29:47 -0700

    update views for displaying events on homepage

---
+++ app/controllers/event.rb
@@ -0,0 +1,13 @@
+get '/event/new' do
+	erb :'events/new'
+end
+
+post '/event/new' do
+	Event.create(host_id: session[:user_id], event_name: params[:event_name], location: params[:location], date: params[:date], start_time: params[:start_time], end_time: params[:end_time])
+	redirect '/'
+end
+
+get '/event/:id' do
+	@events = Event.find(params[:id])
+	erb :'events/show'
+end
