c9766b198a793d73b0ba729966febdb4089885f5
Justincadburywong <justincadburywong@gmail.com>
2016-06-09 15:14:44 -0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-User.create(first_name: "justin", last_name: "wong", password_hash: "123", email: "justin@theinternet.com", address: "429 E Blithedale Ave, Mill Valley, Ca 94941")
-User.create(first_name: "erica", last_name: "freeman", password_hash: "123", email: "erica@theinternet.com", address: "423 E Blithedale Ave, Mill Valley, Ca 94941")
+User.create(first_name: "justin", last_name: "wong", password_hash: "123", email: "justin@theinternet.com", street: "429 E Blithedale Ave", city: "Mill Valley", state: "California", zip: "94941")
+User.create(first_name: "erica", last_name: "freeman", password_hash: "123", email: "erica@theinternet.com", street: "423 E Blithedale Ave", city: "Mill Valley", state: "California", zip: "94941")
 
 20.times do (
 	User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password_hash: "123", email: Faker::Internet.email, street: Faker::Address.street_address, city: Faker::Address.city, state: Faker::Address.state, zip: Faker::Address.zip))
