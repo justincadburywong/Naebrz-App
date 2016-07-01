db309d890ba063ec473b8568371a8c3913f9f9b2
Justincadburywong <justincadburywong@gmail.com>
2016-06-30 21:34:18 -0700

    add comments and logic for error catching

--- app/controllers/event.rb
+++ app/controllers/event.rb
@@ -2,28 +2,38 @@ get '/event/new' do
 	erb :'events/new'
 end
 
+#create a new event
 post '/event/new' do
-	Event.create(host_id: session[:id], event_name: params[:event_name], event_description: params[:event_description], street: params[:street], city: params[:city], state: params[:state], postcode: params[:postcode], start_time: params[:start_time], end_time: params[:end_time])
-	redirect '/'
+	event = Event.new(host_id: session[:id], event_name: params[:event_name], event_description: params[:event_description], street: params[:street], city: params[:city], state: params[:state], postcode: params[:postcode], start_time: params[:start_time], end_time: params[:end_time])
+	if event.save
+    redirect '/'
+  else
+    @error = "You broke the internet.  Please try again!"
+    erb :'/events/new'
+  end
 end
 
+# see an event
 get '/event/:id' do
 	@event = Event.find(params[:id])
 	erb :'events/show'
 end
 
+# page to edit an event
 get '/users/:user_id/event/:id' do
   @event = Event.find(params[:id])
   erb :'/events/edit'
 end
 
+# add a guest to the guestlist
 post '/users/:user_id/event/:id' do
   event = Event.find(params[:id])
-  gatherings = Gatherings.all
-  gatherings.update(event_id: params[:id], guest_id: sessions[:id])
+  guestlist = Guest.all
+  guestlist.update(event_id: params[:id], user_id: sessions[:id])
   redirect "/users/#{params[:user_id]}"
 end
 
+# edit the event
 put '/users/:user_id/event/:id' do
   event = Event.find(params[:id])
   event.update(event_name: params[:event_name], event_description: params[:event_description], street: params[:street], city: params[:city], state: params[:state], postcode: params[:postcode], start_time: params[:start_time], end_time: params[:end_time])
