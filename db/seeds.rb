b33228106a7049c36a9d4fd93398babac5440094
Justincadburywong <justincadburywong@gmail.com>
2016-06-30 21:03:58 -0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# User.create(first_name: "justin", last_name: "wong", password: "123", email: "justin@dbc.com")
+User.create(first_name: "justin", last_name: "wong", password: "123", email: "justin@dbc.com")
 # User.create(first_name: "erica", last_name: "freeman", password_hash: "123", email: "erica@theinternet.com")
 
 20.times do (
@@ -6,8 +6,6 @@
 end
 
 Event.create(event_name: "Sports-ball game", event_description: "Lets get together and watch the sports-ball game at my place!", street: "697 Frederick Street", city: "San Francisco", state: "California", postcode: "94117", start_time: Faker::Time.forward(rand(1..5), :morning), end_time: Faker::Time.forward(rand(6..9), :evening), host_id: rand(1..20))
-
-Event.create(event_name: "Movie night", event_description: "Come to my place and watch a movie on my giant backyard projector!", street: "63 Locust Ave", city: "Mill Valley", state: "California", postcode: "94941", start_time: Faker::Time.forward(rand(1..5), :morning), end_time: Faker::Time.forward(rand(6..9), :evening), host_id: rand(1..20))Event.create(event_name: "Movie night", event_description: "Come to my place and watch a movie on my giant backyard projector!", street: "63 Locust Ave", city: "Mill Valley", state: "California", postcode: "94941", start_time: Faker::Time.forward(rand(1..5), :morning), end_time: Faker::Time.forward(rand(6..9), :evening), host_id: rand(1..20))
 
 Event.create(event_name: "Lakeside Lunch", event_description: "Let's have lunch in Oakland!", street: "KronnerBurger", city: "Oakland", state: "California", postcode: "94611", start_time: Faker::Time.forward(rand(1..5), :morning), end_time: Faker::Time.forward(rand(6..9), :evening), host_id: rand(1..20))
 
