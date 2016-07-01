13e365170b0cd4e495759c045b714d0933e5fb53
Justincadburywong <justincadburywong@gmail.com>
2016-06-30 21:23:46 -0700

    remove gatherings migration and model

--- app/models/gathering.rb
+++
@@ -1,5 +0,0 @@
-class Gathering < ActiveRecord::Base
-  # Remember to create a migration!
-  has_many :guests
-  belongs_to :hosts
-end
--- db/migrate/20160609150738_create_gatherings.rb
+++
@@ -1,8 +0,0 @@
-class CreateGatherings < ActiveRecord::Migration
-  def change
-  	create_table :gatherings do |t|
-  		t.integer :event_id
-  		t.integer :guest_id
-  	end
-  end
-end
