d593de6dad374f91994222bc8b39d3dae06cdbed
Justincadburywong <justincadburywong@gmail.com>
2016-07-01 12:24:09 -0700

    include, but not require 'pony'

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -1,3 +1,5 @@
+# require 'pony'
+
 helpers do
   def current_user
     @current_user ||= User.find_by(id: session[:id])
@@ -7,4 +9,14 @@ helpers do
   	session[:id] = nil
   	current_user = nil
 	end
+
+	def send_email(opts={})
+  # Pony.mail(:to => 'aberger019@gmail.com', :from => 'me@example.com', :subject => 'hi', :body => 'Hello there.')
+  p opts
+  Pony.mail(:to => opts[:to],
+            :from => opts[:from],
+            :subject => opts[:subject], 
+            :body => opts[:body])
+  end
+
 end
