987410a35aa55c8f7a5fe4dd0b048dd179bd3eb3
Justincadburywong <justincadburywong@gmail.com>
2016-09-08 16:12:18 -0700

    update user model specs

--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -1,8 +1,4 @@
 require_relative "spec_helper"
-# require_relative "../app/db/migrate/20160609095832_create_users"
-require "rspec"
-
-# require_relative "../app/db/migrate/20160609095832_create_users.rb"
 
 describe User do
   let(:user1) { User.new }
@@ -37,77 +33,4 @@ describe User do
     end
   end
 
-  # describe "#height" do
-  #   it "has a height" do
-  #     expect(tree.height).to eq 0
-  #   end
-  # end
-
-  # describe "#pass_growing_season" do
-  #   it "should change the age" do
-  #     # This should be more explicit. How much should the tree age by?
-  #     # https://www.relishapp.com/rspec/rspec-expectations/v/2-0/docs/matchers/expect-change
-  #     expect {tree.pass_growing_season}.to change {tree.age}
-  #   end
-
-  #   it "should make the tree grow" do
-  #     #This should be more explicit. How much should the tree grow?
-  #     expect {tree.pass_growing_season}.to change{tree.height}
-  #   end
-
-  #   context "the tree is old enough to bear fruit" do
-  #     it "should cause the tree to produce oranges" do
-  #       expect(tree.mature?).to eq false
-  #       expect(tree2.mature?).to eq true
-  #     end
-  #   end
-  # end
-
-  # describe "#mature?" do
-  #   it "returns true if tree is old enough to bear fruit" do
-  #     expect(tree2.mature?).to eq true
-  #   end
-
-  #   it "returns false if tree is not old enough to bear fruit" do
-  #      expect(tree.mature?).to eq false
-  #   end
-  # end
-
-  # describe "#dead?" do
-  #   it "should return false for an alive tree" do
-  #     expect(tree.dead?).to eq false
-  #   end
-
-  #   it "should return true for a dead tree" do
-  #     expect(tree3.dead?).to eq true
-  #   end
-  # end
-
-  # describe '#has_fruit?' do
-  #   it 'should return true if apples are on the tree' do
-  #     3.times {tree2.pass_growing_season}
-  #     expect(tree2.has_fruit?).to eq true
-  #   end
-
-  #   it "should return false if the tree has no apples" do
-  #     expect(tree.has_fruit?).to eq false
-  #   end
-  # end
-
-  # describe "#pick_a_piee" do
-  #   it "should return an apple from the tree" do
-  #     3.times {tree2.pass_growing_season}
-  #     expect(tree2.pick_a_piece).to be_instance_of(Apple)
-  #   end
-
-  #   it "the returned apple should no longer be on the tree" do
-  #     3.times {tree2.pass_growing_season}
-  #     apple1 = tree2.pick_a_piece
-  #     expect(tree2.fruit).not_to include(apple1)
-  #   end
-
-  #   it "should raise an error if the tree has no apples" do
-  #   expect {tree2.pick_a_piece}.to raise_error StandardError
-  #   end
-  # end
 end
