5e6328ec84a51cdeaf75044f8d696286c3098bd4
Justincadburywong <justincadburywong@gmail.com>
2016-09-08 16:11:01 -0700

    update user model tests

--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -7,7 +7,7 @@ require "rspec"
 describe User do
   let(:user1) { User.new }
   let(:user2) { User.new(first_name: "john", last_name: "doe", email: "john_doe@dbc.com", password: "123") }
-  context "#User Attributes" do
+  context "#Created" do
     it "has a first name" do
       expect(user2.first_name).to eq("john")
     end
@@ -16,6 +16,24 @@ describe User do
     end
     it "has an email" do
       expect(user2.email).to eq("john_doe@dbc.com")
+    end
+    it "has a secure password" do
+      expect(user2.password).to eq("123")
+    end
+  end
+
+  context "#Not Created" do
+    it "does not have a first name" do
+      expect(user1.first_name).to eq nil
+    end
+    it "does not have a last name" do
+      expect(user1.last_name).to eq nil
+    end
+    it "does not have an email" do
+      expect(user1.email).to eq nil
+    end
+    it "does not have a secure password" do
+      expect(user1.password).to eq nil
     end
   end
 
