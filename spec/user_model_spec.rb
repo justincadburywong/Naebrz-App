65a716f89d2204e696e43a824869dd7f103ca0e9
Justincadburywong <justincadburywong@gmail.com>
2016-09-08 18:13:15 -0700

    update user tests for order

--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -3,20 +3,6 @@ require_relative "spec_helper"
 describe User do
   let(:user1) { User.new }
   let(:user2) { User.new(first_name: "john", last_name: "doe", email: "john_doe@dbc.com", password: "123") }
-  context "#Created" do
-    it "has a first name" do
-      expect(user2.first_name).to eq("john")
-    end
-    it "has a last name" do
-      expect(user2.last_name).to eq("doe") 
-    end
-    it "has an email" do
-      expect(user2.email).to eq("john_doe@dbc.com")
-    end
-    it "has a secure password" do
-      expect(user2.password).to eq("123")
-    end
-  end
 
   context "#Not Created" do
     it "does not have a first name" do
@@ -30,6 +16,21 @@ describe User do
     end
     it "does not have a secure password" do
       expect(user1.password).to eq nil
+    end
+  end
+  
+  context "#Created" do
+    it "has a first name" do
+      expect(user2.first_name).to eq("john")
+    end
+    it "has a last name" do
+      expect(user2.last_name).to eq("doe") 
+    end
+    it "has an email" do
+      expect(user2.email).to eq("john_doe@dbc.com")
+    end
+    it "has a secure password" do
+      expect(user2.password).to eq("123")
     end
   end
 
