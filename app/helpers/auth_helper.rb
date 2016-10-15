3a9fb6c31c98b768034c112eefd6ec076125bd48
Justincadburywong <justincadburywong@gmail.com>
2016-10-15 16:24:00 -0700

    add password_match helper

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -10,12 +10,16 @@ helpers do
   	current_user = nil
 	end
 
+  def password_match?
+    if password == password2
+  end
+
 	def send_email(opts={})
   # Pony.mail(:to => 'aberger019@gmail.com', :from => 'me@example.com', :subject => 'hi', :body => 'Hello there.')
   p opts
   Pony.mail(:to => opts[:to],
             :from => opts[:from],
-            :subject => opts[:subject], 
+            :subject => opts[:subject],
             :body => opts[:body])
   end
 
