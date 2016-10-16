a1e2c38bf205a30cef2c492289bc9e371cff52df
Justincadburywong <justincadburywong@gmail.com>
2016-10-15 22:46:38 -0700

    add send_join_text method

--- app/helpers/twilio_helper.rb
+++ app/helpers/twilio_helper.rb
@@ -14,4 +14,20 @@ helpers do
     )
     puts "Sent message to #{@user.first_name} / #{@user.phone}."
   end
+
+  def send_join_text
+    account_sid = TWILIO_SID
+    auth_token = TWILIO_TOKEN
+    client = Twilio::REST::Client.new account_sid, auth_token
+    @event = Event.find(@guest.event_id)
+    @host = User.find(@event.host_id)
+    from = "4152752620"
+
+      client.account.messages.create(
+        :from => from,
+        :to => current_user.phone,
+        :body => "This is a reminder that you've joined #{@host.first_name}'s event!  Don't forget to be available at #{@event.start_time}"
+      )
+      puts "Sent message to #{current_user.first_name} / #{current_user.phone}."
+  end
 end
