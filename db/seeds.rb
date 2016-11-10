b76eade21cd9c02aa04fa9e9a5f43869d814ec6b
Justincadburywong <justincadburywong@gmail.com>
2016-11-10 12:42:40 -0800

    edit seeds to change guestlist

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,5 +20,5 @@ Event.create(event_name: "Dinner by the water", event_description: "I want to go
 Event.create(event_name: "Hike through Muir Woods", event_description: "Let's go walk through the mist at Muir Woods!", street: "Muir Woods", city: "Mill Valley", state: "California", postcode: "94941", start_time: Faker::Time.forward(rand(1..5), :morning), end_time: Faker::Time.forward(rand(6..9), :evening), host_id: rand(1..20))
 
 30.times do (
-	Guest.create(user_id: rand(1..20), event_id: rand(1..7)))
+	Guest.create(user_id: rand(2..20), event_id: rand(1..7)))
 end
