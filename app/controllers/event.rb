398f6f75667466599904b5dde8aea192b22a3474
Justincadburywong <justincadburywong@gmail.com>
2016-11-16 23:33:08 -0800

    update delete event to notify all guests via email

--- app/controllers/event.rb
+++ app/controllers/event.rb
@@ -2,7 +2,7 @@ get '/events/new' do
 	erb :'events/new'
 end
 
-#create a new event
+## create a new event
 post '/events' do
 	event = Event.new(host_id: current_user.id, event_name: params[:event_name], event_description: params[:event_description], street: params[:street], city: params[:city], state: params[:state], postcode: params[:postcode], start_time: params[:start_time], end_time: params[:end_time])
 	if event.save
@@ -13,7 +13,7 @@ post '/events' do
   end
 end
 
-# see an event
+## see an event
 get '/events/:id' do
 	@event = Event.find(params[:id])
   @guestlist = @event.guests
@@ -23,27 +23,27 @@ get '/events/:id' do
 	erb :'events/show'
 end
 
-# page to edit an event
+## page to edit an event
 get '/users/:user_id/events/:id' do
   @event = Event.find(params[:id])
   erb :'/events/edit'
 end
 
-# edit the event
+## edit the event
 put '/users/:user_id/events/:id' do
   event = Event.find(params[:id])
   event.update(event_name: params[:event_name], event_description: params[:event_description], street: params[:street], city: params[:city], state: params[:state], postcode: params[:postcode], start_time: params[:start_time], end_time: params[:end_time])
   redirect "/users/#{params[:user_id]}"
 end
 
-# add yourself to the guestlist
+## add yourself to the guestlist
 post '/users/:user_id/events/:id/guests' do
   @guest = Guest.create(event_id: params[:id], user_id: current_user.id)
-	send_join_text
+	# send_join_text
   redirect "/events/#{params[:id]}"
 end
 
-# remove yourself from the guestlist
+## remove yourself from the guestlist
 post '/users/:user_id/events/:id/guest' do
 	@me = Guest.find_by(user_id: current_user.id, event_id: params[:id])
 	@me.destroy
@@ -52,6 +52,11 @@ end
 
 delete '/users/:user_id/events/:id' do
   @event = Event.find(params[:id])
+	@user = User.find(params[:user_id])
+	@guests = []
+	@event.guests.each do |x| @guests << x.user_id end
+	## send everybody a cancelation email
+	pony_cancel_event_email
   @event.destroy
   redirect "/users/#{params[:user_id]}"
 end
