fe4cd1f7054cef3c9b571e18bdd845801c13c16e
Justincadburywong <justincadburywong@gmail.com>
2020-04-30 11:34:14 -0700

    043020 remove segment constant from helpers

--- app/helpers/segment_helper.rb
+++ app/helpers/segment_helper.rb
@@ -1,9 +1,9 @@
-helpers do
-  def segment_identify
-    if current_user
-      Analytics.identify(
-        user_id: current_user.id,
-        traits: { email: current_user.email})
-    end
-  end
-end
+# helpers do
+#   def segment_identify
+#     if current_user
+#       Analytics.identify(
+#         user_id: current_user.id,
+#         traits: { email: current_user.email})
+#     end
+#   end
+# end
