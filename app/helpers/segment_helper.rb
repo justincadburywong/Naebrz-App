5e5de893608e7cdfc1ed829b569e0a6643ec42da
Justincadburywong <justincadburywong@gmail.com>
2020-04-30 11:37:58 -0700

    043020 delete segment from app

--- app/helpers/segment_helper.rb
+++
@@ -1,9 +0,0 @@
-# helpers do
-#   def segment_identify
-#     if current_user
-#       Analytics.identify(
-#         user_id: current_user.id,
-#         traits: { email: current_user.email})
-#     end
-#   end
-# end
